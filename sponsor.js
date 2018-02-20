3ea22e615d49317c77279efd431ad1d8e80c2141
taufiqnurh <36594804+taufiqnurh@users.noreply.github.com>
2018-02-20 22:56:16 +0700

    Update sponsor.js

--- sponsor.js
+++ sponsor.js
@@ -1,1 +1,27 @@
- 
+<!DOCTYPE html>
+<html>
+<body>
+
+<br>
+<br>
+<br>
+
+<button onclick="myFunction()">Klik untuk melihat Info Hari ini</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var date = new Date().getDate(); 
+    var greeting;
+    if (date > 20 ) {
+        greeting = "Info: Pengantar Ayat Ekonomi Libur";
+    } else {
+        greeting = "Tidak ada Info";
+    }
+    document.getElementById("demo").innerHTML = greeting;
+}
+</script>
+
+</body>
+</html>
