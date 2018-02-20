97a222a4f0910c92a5353d9efc79ce5d85e515f7
taufiqnurh <36594804+taufiqnurh@users.noreply.github.com>
2018-02-21 06:28:34 +0700

    Update sponsor.js

--- sponsor.js
+++ sponsor.js
@@ -1,8 +1,1 @@
-var date = new Date().getDate(); 
-    var greeting;
-    if (date > 20 ) {
-        greeting = "Info: Pengantar Ayat Ekonomi Libur";
-    } else {
-        greeting = "tidak ada info";
-    }
-    document.getElementById("demo").innerHTML = greeting;
+
